b80cacd7932d21ed593c2eaec7a3e86dd54e50fe
Jennifer <jenniferzheng23@gmail.com>
2018-03-06 04:12:08 +0000

    finish

--- js/script.js
+++ js/script.js
@@ -23,27 +23,27 @@
 //	"imageurl":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 //}
 
-var myPlayList = [
-	{
-		"title":"24K Magic",
-		"artist":"Bruno Mars",
-		"mp3url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"imageurl":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
-	},
-	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"imageurl":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
-	},
-	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"imageurl":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
-	}
-
-]
+    var myPlayList = [
+    	{
+    		"title":"24K Magic",
+    		"artist":"Bruno Mars",
+    		"mp3url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+    		"imageurl":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+    	},
+    	{
+    		"title":"Sir Duke",
+    		"artist":"Stevie Wonder",
+    		"mp3url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
+    		"imageurl":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+    	},
+    	{
+    		"title":"Sorry",
+    		"artist":"Justin Bieber",
+    		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
+    		"imageurl":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+    	}
+    
+    ]
 
 
 
@@ -57,68 +57,68 @@ $( document ).ready(function() {
 //	$('.songs').append("<p>Artist: " + myPlayList.artist + "</p>");
 //	$('.songs').append("<p>MP3: <a href=" +myPlayList.mp3url+ "> 24K Magic </a></p>");
 //	$('.songs').append("<img src=" + myPlayList.imageurl + ">");
-	myPlayList.forEach(function(songs) {
-    var title = songs.title;
-    var artist = songs.artist;
-    var url = songs.mp3url;
-    var image = songs.imageurl;
+//	myPlayList.forEach(function(songs) {
+//    var title = songs.title;
+//    var artist = songs.artist;
+//    var url = songs.mp3url;
+//    var image = songs.imageurl;
  
-   $("body").append(`
-		<div class="songs">
-        	<h3>Title: ${title}</h2>
-        	<a>Artist: ${artist}</a>
-        	<img src="${image}">
-        	<a href= ${url}> Play Song</a> 
-       </div>
-`);
-});
-
-
-
-});
-$('#addSong').click(function(){
-    addSong();
-    displayList();
-    console.log("hello");
+//       $("body").append(`
+//    		<div class="songs">
+//            	<h3>Title: ${title}</h3>
+//            	<a>Artist: ${artist}</a>
+//            	<img src="${image}">
+//            	<a href= ${url}> Play Song</a> 
+//           </div>
+//        `);
+        
+//    });
     
-});
-$('#clear').click(function(){
-    clearList();
-});
-
-function displayList(){
-	for (var i = 0; i < myPlayList.length; i++){
-        $('.songs').append('<p>' + myPlayList[i].title+'<p>');
-        $('.songs').append('<p>' + myPlayList[i].artist +'<p>');
-        $('.songs').append('<p>' + myPlayList[i].mp3url +'<p>');
-        $('.songs').append('<p>' + myPlayList[i].imageurl +'<p>');
+    $('#addSong').click(function(){
+        clearList();
+        addSong();
+        displayList();
+     
+    });
+    
+    $('#clear').click(function(){
+        clearList();
+       
+    });
+    
+    function displayList(){
+        
+    	for (var i = 0; i < myPlayList.length; i++){
+            $('.songs').append('<p>' + myPlayList[i].title+'<p>');
+            $('.songs').append('<p>' + myPlayList[i].artist +'<p>');
+            $('.songs').append("<p>MP3: <a href=" +myPlayList[i].mp3url+ ">Link</a></p>");
+            $('.songs').append("<img src=" + myPlayList[i].imageurl + ">");
+        }
     }
-
-  
-}
-
-function clearList(){
-   $('.songs').empty();
-  
-  
-}
-
-function addSong(){
-	var titleInput = $("#title").val();
-    var artistInput = $("#artist").val();
-    var urlInput = $("#mp3url").val();
-    var imageInput = $("#imageurl").val();
     
-    var song = {
-        title: titleInput,
-        artist: artistInput,
-        url: urlInput,
-        image: imageInput
-        };
-    playList.push(song);
-}
+    function clearList(){
+       $('.songs').empty();
+      
+    }
+    
+    function addSong(){
+    	var titleInput = $("#title").val();
+        var artistInput = $("#artist").val();
+        var urlInput = $("#mp3url").val();
+        var imageInput = $("#imageurl").val();
+        
+        var song = {
+            title: titleInput,
+            artist: artistInput,
+            url: urlInput,
+            image: imageInput
+            };
+        myPlayList.push(song);
+        
+        
+    }
+    
+    displayList();
 
-displayList();
-
-  
+});
 
